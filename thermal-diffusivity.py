f9de3eea19e87b7a0aac3251f1915973568f6353
alcaemre <emre.alca@icloud.com>
2023-01-31 14:55:39 -0500

    attempt at automating finding good parameters

--- thermal-diffusivity.py
+++ thermal-diffusivity.py
@@ -3,6 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as op
 import scipy.special as sp
+import itertools
+import threading
+import time
+import sys
 
 
 
@@ -49,7 +53,7 @@ def J_0 (x, A, f):
     k = 50
     J = 0
     for i in range(k):
-        J = A*(((-1)**i)/(sp.factorial(i))**2)*((x**2)/(4*f))**i + ber
+        J += A*(((-1)**i)/(sp.factorial(i))**2)*((x**2)/(4*f))**i
     return J
 
 def ber_0(x, A, f, offset):
@@ -63,42 +67,79 @@ def ber_0(x, A, f, offset):
     k = 10
     ber = 0
     for i in range(k):
-        ber += ((np.cos((i * np.pi)/2))/(sp.factorial(i))**2)*((x**2)/(4*f))**i     # calculate bessel function, f = omega*r**3/m
-    ber = (A*ber) + offset                                                        # multiple the bessel function by some amplitude (A) and add some vertical offset (offset)
+        ber += ((np.cos((i * np.pi)/2))/(sp.factorial(i))**2)*((x**2)*f)**i     # calculate bessel function, f = m/(omega*(r**2))
+    ber = (A*ber) + offset                                                      # multiple the bessel function by some amplitude (A) and add some vertical offset (offset)
     return ber
+
+def find_optimal_params(trial):
+    A_s = np.linspace(0, 20, 20)
+    f_s = np.linspace(10, 100000, 20)
+    offset_s = np.linspace(20, 70, 10)
+    chars = "/—\|"
+    chars_i = 0
+    
+    p0s = {}
+    perrs = []
+    for A in A_s:
+        for f in f_s:
+            for offset in offset_s:
+
+                sys.stdout.write('\r'+'calculating . . .  '+chars[chars_i % 4]) # animating output to show that function is running
+                chars_i += 1
+                
+                popt, pcov = op.curve_fit(ber_0, trial[0], trial[1])
+                perr = np.sqrt(np.diag(pcov))[0]
+                # print(perr)
+                perrs.append(perr)
+                p0s[str(perr)]=(A, f, offset)
+
+                sys.stdout.flush() # clearing animation
+    
+    return p0s[str(np.min(perrs))]
 
 
 if __name__ == "__main__":
 
-    trial1 = get_wave_from_csv(r'trial1.csv')
+    plotting = True
+    optimizing = False
+
+    # trial1 = get_wave_from_csv(r'trial1.csv')
     # trial2 = get_wave_from_csv(r'trial2.csv')
     # trial3 = get_wave_from_csv(r'trial3.csv')
-    # print(trial1)
 
-    fig, (ax1,ax2, ax3) = plt.subplots(3,1)
-    ax2.set_ylabel('temperature')
-    ax3.set_xlabel('time')
+    if plotting:
+        fig, (ax1,ax2, ax3) = plt.subplots(3,1)
+        ax2.set_ylabel('temperature')
+        ax3.set_xlabel('time')
 
-    axs= [ax1, ax2, ax3]
+        axs= [ax1, ax2, ax3]
 
-    p0s=[()]
+        p0s=[(20.0, 100000.0, 70.0), (20.0, 100000.0, 70.0), (10, 10000000, 50)]
 
-    for i in range(3):
-        trial_label = rf'trial{i+1}'
-        # print(trial_label)
-        axs[i].set_title(trial_label)
+        for i in range(3):
+            trial_label = rf'trial{i+1}'
+            # print(trial_label)
+            axs[i].set_title(trial_label)
 
-        trial = get_wave_from_csv(trial_label+'.csv')
-        # print(trial)
-        axs[i].plot(trial[0], trial[1], label='T_I')
-        axs[i].plot(trial[0], trial[2], label='T_S')
-        popt, pcov = op.curve_fit(ber_0, trial[0], trial[1], p0=(10, 600, 50))
-        print(popt)
-        axs[i].plot(trial[0], ber_0(trial[0], popt[0], popt[1], popt[2]), label='bessel function')
+            trial = get_wave_from_csv(trial_label+'.csv')
+            # print(trial)
+            axs[i].plot(trial[0], trial[1], label='T_I')
+            axs[i].plot(trial[0], trial[2], label='T_S')
+            popt, pcov = op.curve_fit(ber_0, trial[0], trial[1], p0=p0s[i])
+            # print(popt)
+            print(pcov**2)
+            axs[i].plot(trial[0], ber_0(trial[0], popt[0], popt[1], popt[2]), label='bessel function')
 
-        axs[i].legend(loc=1)
+            axs[i].legend(loc=3)
 
-plt.show()
+        plt.show()
+
+    # automating f**king around for parameters
+    if optimizing:
+        trial = get_wave_from_csv(rf'trial3.csv')
+        print(find_optimal_params(trial))
+
+
 
 
 # xx=np.linspace(0, 30, 100)
