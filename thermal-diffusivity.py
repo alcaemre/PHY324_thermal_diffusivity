705c5b21cdcdaf9ce8ea2d5a1d23be463286cc9f
jacealloway <124064017+jacealloway@users.noreply.github.com>
2023-02-04 12:51:36 -0500

    chi2 fit analysis on each of the fitted trials

--- thermal-diffusivity.py
+++ thermal-diffusivity.py
@@ -3,13 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.optimize as op
 import scipy.special as sp
+from scipy.stats import chi2
 import itertools
 import threading
 import time
 import sys
 
 
-
+    #load data
 def get_wave_from_csv(file):
     """Parses .csv files in the expected format and returns a list of np.array's holding
      the time of measurement (t),
@@ -41,7 +42,7 @@ def get_wave_from_csv(file):
             T_S[i] = T_I[0]
     return [t,T_I, T_S]
 
-
+    #define bessel function
 def J_0 (x, A, f, offset):
     """real componant of the bessel function
 
@@ -57,6 +58,7 @@ def J_0 (x, A, f, offset):
     J = A*J + offset
     return J
 
+    #define real component of kelvin function
 def ber_0(x, A, f, offset):
     """real componant of the bessel function
 
@@ -72,8 +74,7 @@ def ber_0(x, A, f, offset):
     ber = (A*ber) + offset                                                      # multiple the bessel function by some amplitude (A) and add some vertical offset (offset)
     return ber
 
-
-
+    #define complex component of kelvin function
 def ber_i(x, A, f, offset):
     """real componant of the bessel function
 
@@ -89,7 +90,7 @@ def ber_i(x, A, f, offset):
     ber = (A*ber) + offset                                                      # multiple the bessel function by some amplitude (A) and add some vertical offset (offset)
     return ber
 
-
+    #animation for finding optimal parameters
 def find_optimal_params(trial):
     A_s = np.linspace(0, 20, 20)
     f_s = np.linspace(10, 100000, 20)
@@ -117,6 +118,7 @@ def find_optimal_params(trial):
     return p0s[str(np.min(perrs))]
 
 
+    #plotting / animating
 if __name__ == "__main__":
 
     plotting = True
@@ -156,11 +158,15 @@ if __name__ == "__main__":
         trial2 = get_wave_from_csv('trial2.csv')
         trial3 = get_wave_from_csv('trial3.csv')
 
+        trial1_error = 2
+        trial2_error = 2
+        trial3_error = 2
+
     #trial 1 fitting attempt
         axs[0].set_title('trial 1 - fitting attempt')
         axs[0].plot(trial1[0], trial1[1], label='T_I')
         axs[0].plot(trial1[0], trial1[2], label='T_S')
-        axs[0].errorbar(trial1[0], trial1[1], yerr=2,  label='temp T_I error', color='k', fmt='none', capsize=1, lw=0.5) #add our errorbars
+        axs[0].errorbar(trial1[0], trial1[1], yerr=trial1_error,  label='temp T_I error', color='k', fmt='none', capsize=1, lw=0.5) #add our errorbars
         popt1, pcov1 = op.curve_fit(J_0, trial1[0], trial1[1], p0=p0s[0])
             # print(popt)
             # print(pcov3**2)
@@ -172,7 +178,7 @@ if __name__ == "__main__":
         axs[1].set_title('trial 2 - fitting attempt')
         axs[1].plot(trial2[0], trial2[1], label='T_I')
         axs[1].plot(trial2[0], trial2[2], label='T_S')
-        axs[1].errorbar(trial2[0], trial2[1], yerr=2,  label='temp T_I error', color='k', fmt='none', capsize=1, lw=0.5) #add our errorbars
+        axs[1].errorbar(trial2[0], trial2[1], yerr=trial2_error,  label='temp T_I error', color='k', fmt='none', capsize=1, lw=0.5) #add our errorbars
         popt2, pcov2 = op.curve_fit(J_0, trial2[0], trial2[1], p0=p0s[1])
             # print(popt)
             # print(pcov3**2)
@@ -183,17 +189,61 @@ if __name__ == "__main__":
         axs[2].set_title('trial 3 - sinusoidal superposition')
         axs[2].plot(trial3[0], trial3[1], label='T_I')
         axs[2].plot(trial3[0], trial3[2], label='T_S')
-        axs[2].errorbar(trial3[0], trial3[1], yerr=2,  label='temp T_I error', color='k', fmt='none', capsize=1, lw=0.5)
+        axs[2].errorbar(trial3[0], trial3[1], yerr=trial3_error,  label='temp T_I error', color='k', fmt='none', capsize=1, lw=0.5)
         popt3, pcov3 = op.curve_fit(J_0, trial3[0], trial3[1], p0=p0s[2]) #curve_fit
             # print(popt)
             # print(pcov3**2)
-        #plotting the curve fit bessel function
         axs[2].plot(trial3[0], (2**(trial3[0]/700))*J_0(trial3[0], popt3[0], popt3[1], 0)-20*np.sin(trial3[0]/400)+popt3[2]+10, label='bessel fit')
         axs[2].legend(loc='upper right')
 
         plt.subplots_adjust(hspace=0.5)
         plt.show()
 
+
+    #try to compare with the measured thermal diffusivity of rubber
+    # m_expected = 0.089 mm^2/s
+
+    #ideal fit slices - slice the acquired data to where the frequency/amplitude fits best fit the taken data
+    slice1 = [27, 59]
+    slice2 = [38, 90]
+    slice3 = [28, 57]
+
+#begin chi2 analysis
+    n1 = trial1[1][slice1[0]:slice1[1]]
+    n1_fit = (1.2**(trial1[0]/700))*J_0(trial1[0], popt1[0], popt1[1], 0)+16*np.sin(trial1[0]/400)-10+popt1[2]
+    n1_fit = n1_fit[slice1[0]:slice1[1]]
+    chi2_1 = np.sum((n1 - n1_fit)**2/(trial1_error**2))
+    dof_1 = len(n1) - len(popt1)
+    prob1 = 1 - chi2.cdf(chi2_1, dof_1)
+
+    n2 = trial2[1][slice2[0]:slice2[1]]
+    n2_fit = J_0(trial2[0]-10, popt2[0], popt2[1], 0) + 10*np.sin(trial2[0]/400) -3 + popt2[2]
+    n2_fit = n2_fit[slice2[0]:slice2[1]]
+    chi2_2 = np.sum((n2 - n2_fit)**2/(trial2_error**2))
+    dof_2 = len(n2) - len(popt2)
+    prob2 = 1 - chi2.cdf(chi2_2, dof_2)
+
+    n3 = trial3[1][slice3[0]:slice3[1]]
+    n3_fit = (2**(trial3[0]/700))*J_0(trial3[0], popt3[0], popt3[1], 0)-20*np.sin(trial3[0]/400)+popt3[2]+10
+    n3_fit = n3_fit[slice3[0]:slice3[1]]
+    chi2_3 = np.sum((n3 - n3_fit)**2/(trial3_error**2))
+    dof_3 = len(n3) - len(popt3)
+    prob3 = 1 - chi2.cdf(chi2_3, dof_3)
+
+    #extract values
+print('the probability of the trial 1 slice being a good fit is', prob1, 'with frequency', popt1[1])
+print('the probability of the trial 2 slice being a good fit is', prob2, 'with frequency', popt2[1])
+print('the probability of the trial 3 slice being a good fit is', prob3, 'with frequency', popt3[1])
+
+
+
+
+    #attempt to determine the value of m from the extracted frequency values:
+r = 15.85 - 6.38    #in mm
+r_error = 0.1       #this is just 0.05mm + 0.05mm
+
+omega_applied_1 = 60    #in seconds
+omega1_error = 2    #not sure about this value yet
 
 
 
